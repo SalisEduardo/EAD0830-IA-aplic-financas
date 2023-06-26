047a299227981202a8c4ce676496c8f263ffd67e
Eduardo <amorimsalis.edu@usp.br>
2023-06-25 22:47:08 -0300

    Predicting for validation

--- Atividades/atv2/utils/metrics.py
+++ Atividades/atv2/utils/metrics.py
@@ -43,6 +43,44 @@ def get_metrics(model,y_true,X_true,probs=None,predictions=None):
 
 
 
+def get_metrics_table(model,y_true,X_true,model_name,probs=None,predictions=None):
+    if predictions is None:
+        y_pred =  model.predict(X_true)
+    else:
+        y_pred = predictions
+
+    if probs is None:
+        y_pred_proba =  model.predict_proba(X_true)
+    else:
+        y_pred_proba = probs
+
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred_proba[:,-1] )
+    auc = roc_auc_score(y_true, y_pred_proba[:,-1] )
+    cm = confusion_matrix(y_true, y_pred)
+    report = classification_report(y_true, y_pred)
+
+    metrics = {
+        "Model":model_name,
+        'Accuracy' : accuracy,
+        'Precision' : precision,
+        'Recall' : recall,
+        'F1-Score' : f1,
+        'Area Under the Curve': auc,
+        "Gini": round(2*auc-1,2)
+
+    }
+
+    df = pd.DataFrame([metrics])
+
+    return df
+
+
+
+
 
 
 def  display_metrics(train_metrics_report,test_metrics_report,not_show=['Confussion Matrix','Classification Report','False Positive Ratio','True Positive Ratio','Thresholds']):
